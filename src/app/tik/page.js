5cca217d2e2c99241770922b4969366d044cd72a
flou-ainan <flouainan@gmail.com>
2024-05-16 01:59:43 -0300

    first complete version

---
+++ src/app/tik/page.js
@@ -0,0 +1,24 @@
+import LinkList from "@/app/components/LinkList";
+
+export default function Home() {
+  const config = {
+    url_foto_perfil: "https://i.ibb.co/HFYK4t6/perfil.png",
+    titulo: "Enacless",
+    subtitulo: "Bolsas Clássicas em Crochê",
+    links:[
+      {
+        texto: "Tutoriais Gratuitos - Youtube",
+        url: "https://www.youtube.com/watch?v=m6xkmyNA_g4&list=PLmAcT57iO9YVP2bpPNjlvyPISFyhhYk0N"
+      },{
+        texto: "Meu Instagram",
+        url: "https://www.instagram.com/ateliemarienacles/"
+      },{
+        texto: "Grupo Vip Telegram",
+        url: "https://t.me/marianaenacles"
+      }
+    ]
+  }
+  return (
+    <LinkList config={config} />
+  );
+}
