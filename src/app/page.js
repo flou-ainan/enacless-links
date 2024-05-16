476d2117aec1326a604f7ae4080696a97a4f93c1
flou-ainan <flouainan@gmail.com>
2024-05-16 01:45:29 -0300

    enacless

--- src/app/page.js
+++ src/app/page.js
@@ -3,18 +3,18 @@ import LinkList from "./components/LinkList";
 export default function Home() {
   const config = {
     url_foto_perfil: "https://i.ibb.co/HFYK4t6/perfil.png",
-    titulo: "Mariana Enacles",
+    titulo: "Enacless",
     subtitulo: "Bolsas Clássicas em Crochê",
     links:[
       {
         texto: "Grupo Exclusivo - Novidades",
-        url: ""
+        url: "https://chat.whatsapp.com/CPjCrwm6b3f1wROOfonjFr"
       },{
         texto: "Atendimento Whatsapp",
-        url: ""
+        url: "https://wa.me/message/UIMF2D2QMBGVB1"
       },{
         texto: "Acessar Catálogo",
-        url: ""
+        url: "https://wa.me/5535998026142?text=Ol%C3%A1%2C%20pode%20me%20enviar%20o%20cat%C3%A1logo%3F%0A"
       }
     ]
   }
